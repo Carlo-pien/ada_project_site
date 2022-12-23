e765c6266acbeae78322691b034466df809e7c82
Carlo-pien <56826936+Carlo-pien@users.noreply.github.com>
2022-12-23 16:07:18 +0100

    Case studies

--- js/scripts.js
+++ js/scripts.js
@@ -92,8 +92,63 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
+        content: '<div id="myModal" class="modal"><div class="modal-content"><div class="modal-header"><span class="btn-close"></span></div><div class="container"><h2 class="modal-title">Title of Case study</h2><hr class="divider" /><div class="modal-body"><div class="row gx-4 gx-lg-5 justify-content-center"><div class="col-lg-8 text-center" ><p class=" mb-4 text-black-75 ">Test textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest textTest text</p><img src="assets/figures/mobility_timeseries.png" width="650" ></div></div></div></div></div></div>',
         elements: '#portfolio a.portfolio-box'
+
     });
+
+
+    
 
 })
 
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("btn-close")[0];
+var span2 = document.getElementsByClassName("btn-close")[1];
+var span3 = document.getElementsByClassName("btn-close")[2];
+
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+    modal2.style.display = "block";
+  }
+// When the user clicks on the button, open the modal
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+span3.onclick = function() {
+    modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal || event.target == modal2 || event.target == modal3 ) {
+    modal.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+  }
+}
